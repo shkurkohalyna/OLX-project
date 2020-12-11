6b05020199f44ca43415f9bdd3d0e42b899119a5
Shoor1k <qipshoor1k@gmail.com>
2020-12-11 17:01:07 +0200

    разметка и все модалки с хедера

--- src/js/header-modal.js
+++ src/js/header-modal.js
@@ -2,17 +2,23 @@ import { API_OLX } from './url';
 import fetchCategory from './fetchCategory';
 import MarkupSideNavDesctop from '../templates/header-sidenav-desctop.hbs'
 import MarkupSidenavMobile from '../templates/header-sidenav-mobile.hbs'
-// открытие - закрытие модалок Хедера
+import MarkupModalCreateAnAdCategory from '../templates/header-create-an-ad-category.hbs'
+
 const openSideNav = document.querySelector('[data-menu-button]');
 const mobileMenuRef = document.querySelector('[data-menu]');
 const clouseSideNav = document.querySelector(`[data-clouse-button]`)
 const openCreateAnAd = document.querySelector(`[data-create-an-ad-btn-open]`)
 const modalCreateAnAd = document.querySelector(`[data-create-ad]`)
 const clouseModalCreateAnAd = document.querySelector(`[data-clouse-button-create-ad]`)
-const imageInput = document.querySelector(`.js-inputClick`)
-const sideNavDesctop = document.querySelector([`#sidenav-desctop`])
-
-
+const sideNavDesctop = document.querySelector(`#sidenav-desctop`)
+const changeCategoryForCreateAnAd = document.querySelector(`#categori-product-add-list`)
+const openModalRegistrartion = document.querySelector(`[data-modal-open-registration]`)
+const modalRegistration = document.querySelector(`[data-menu-registration]`)
+const clouseModalRegistration = document.querySelector(` [data-clouse-button-modal-registration]`)
+const sidenavModalRegistrartion = document.querySelector(`[data-sidenav-open-registration]`)
+// data-sidenav-open-registration
+// открытие - закрытие модалок Хедера
+console.log();
 function onOpenModalHeader(btn, modal) {
         btn.addEventListener(`click`, openModal)
         function openModal() {
@@ -23,17 +29,29 @@ onOpenModalHeader(openSideNav, mobileMenuRef)
 onOpenModalHeader(clouseSideNav, mobileMenuRef)
 onOpenModalHeader(openCreateAnAd, modalCreateAnAd)
 onOpenModalHeader(clouseModalCreateAnAd, modalCreateAnAd)
+onOpenModalHeader(openModalRegistrartion, modalRegistration)
+onOpenModalHeader(clouseModalRegistration, modalRegistration)
+onOpenModalHeader(sidenavModalRegistrartion,modalRegistration)
 
-
-// превью картинок
-
- // sidenav(Desctop) отрисовка
+ // sidenav(Desctop) рендер категорий
 fetchCategory(API_OLX).then(responce => appendSideNavDesctop(responce))
 function appendSideNavDesctop(cat) {
         sideNavDesctop.innerHTML = MarkupSideNavDesctop(cat)
 }
-// sidenav(mobile-tablet) отрисовка
-// fetchCategory(API_OLX).then(responce => appendSideNavMobile(responce))
-// function appendSideNavMobile(cat) {
-//         mobileMenuRef.innerHTML = MarkupSidenavMobile(cat)
-// }
+// sidenav(mobile-tablet) рендрер категорий
+fetchCategory(API_OLX).then(responce => appendSideNavMobile(responce))
+function appendSideNavMobile(cat) {
+        mobileMenuRef.firstElementChild.insertAdjacentHTML(`beforeend`, MarkupSidenavMobile(cat)) 
+}
+
+//Создать обьявление рендер категорий
+fetchCategory(API_OLX).then(responce => appendchangeCategoryForCreateAnAd(responce))
+function appendchangeCategoryForCreateAnAd(cat) {
+        changeCategoryForCreateAnAd.innerHTML = MarkupModalCreateAnAdCategory(cat)
+}
+
+
+
+// для включение меню авторизованого пользывателя включить клас " is-login" на:
+// <button class="sidenav__registry">
+//  <div class="sidenav__menu-acount .is-login">
