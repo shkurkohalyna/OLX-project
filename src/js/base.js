6afca62c822d086e463b4f1ddbb1e021d7126ca2
Artem <64410376+Artiss90@users.noreply.github.com>
2020-12-11 23:30:05 +0200

    GetUser

--- src/js/base.js
+++ src/js/base.js
@@ -7,6 +7,7 @@ import fetchRegistration from './fetch/fetchRegistration';
 import fetchAuthenticationLogin from './fetch/fetchAuthenticationLogin';
 import { fetchLogout } from './fetch/fetchLogout';
 import { fetchCall } from './fetch/fetchCall';
+import { fetchGetUser } from './fetch/fetchGetUser';
 
 const newUser = {
     "email": "artiss@example.com",
@@ -16,7 +17,9 @@ fetchCategory(API_OLX).then(console.log)
 // fetchRegistration(API_OLX, newUser).then(console.log)
 fetchAuthenticationLogin(API_OLX, newUser).then(response => localStorage.setItem('key', `${response.accessToken}`))
 fetchAuthenticationLogin(API_OLX, newUser).then(console.log)
-fetchLogout(API_OLX).then(console.log)
 fetchCall(API_OLX, 1).then(console.log)
 fetchCall(API_OLX, 2).then(console.log)
 fetchCall(API_OLX, 3).then(console.log)
+fetchGetUser(API_OLX).then(console.log)
+
+fetchLogout(API_OLX).then(console.log)
--- src/js/fetch/fetchCall.js
+++ src/js/fetch/fetchCall.js
@@ -1,10 +1,8 @@
 export async function fetchCall(url,page) {
-  const key = localStorage.getItem('key');
   const options = {
     method: 'GET',
     headers: {
-       'accept': 'application/json',
-      'Authorization': `${key}`,
+       'accept': 'application/json'
     },
   };
   const response = await fetch(`${url}call?page=${page}`, options);
